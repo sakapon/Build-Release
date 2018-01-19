afa2f3f48d778193949fa3cc40341a01bb1f0a3c
Keiho Sakapon <git@saka-pon.net>
2018-01-19 18:17:47 +0900

    Update project settings

--- Lab/2018/BuildSample/VersionIncrementConsole/Properties/AssemblyInfo.cs
+++ Lab/2018/BuildSample/VersionIncrementConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("VersionIncrementConsole")]
+[assembly: AssemblyTitle("Version Increment - Console App")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VersionIncrementConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2018 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+
+[assembly: InternalsVisibleTo("UnitTest")]
