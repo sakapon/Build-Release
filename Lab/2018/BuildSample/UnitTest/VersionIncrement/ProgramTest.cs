e281c6b80c8b9c5fefe18f5a4fa35beb988385be
Keiho Sakapon <git@saka-pon.net>
2018-01-20 09:07:41 +0900

    Add test

---
+++ Lab/2018/BuildSample/UnitTest/VersionIncrement/ProgramTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.VersionIncrement
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void IncrementLine_1()
+        {
+            void IncrementLine_Test(string line, string expected)
+            {
+                var actual = Program.IncrementLine(line);
+                Assert.AreEqual(expected, actual);
+            }
+
+            IncrementLine_Test(
+                "[assembly: AssemblyVersion(\"1.0.0.0\")]",
+                "[assembly: AssemblyVersion(\"1.0.1.0\")]");
+            IncrementLine_Test(
+                "[assembly: AssemblyFileVersion(\"1.23.456\")]",
+                "[assembly: AssemblyFileVersion(\"1.23.457\")]");
+            IncrementLine_Test(
+                "// [assembly: AssemblyVersion(\"1.0.*\")]",
+                "// [assembly: AssemblyVersion(\"1.0.*\")]");
+            IncrementLine_Test(
+                "[assembly: AssemblyCompany(\"Xyz Company\")]",
+                "[assembly: AssemblyCompany(\"Xyz Company\")]");
+        }
+    }
+}
