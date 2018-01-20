8c11ee9c54c8be8f8f3e23fa86c9bfa207e43a79
Keiho Sakapon <git@saka-pon.net>
2018-01-21 00:11:17 +0900

    Create project

---
+++ Lab/2018/BuildSample/ZipReleaseConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipReleaseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
