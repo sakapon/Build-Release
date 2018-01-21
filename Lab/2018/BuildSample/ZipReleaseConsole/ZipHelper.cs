d794cd108758bf196e44d9a8229bde30d462c4e6
Keiho Sakapon <git@saka-pon.net>
2018-01-21 13:52:51 +0900

    Copy source code

---
+++ Lab/2018/BuildSample/ZipReleaseConsole/ZipHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+public static class ZipHelper
+{
+    public static void CreateZipForAssembly(string sourceAssemblyFilePath, string targetDirPath)
+    {
+        var assemblyName = Path.GetFileNameWithoutExtension(sourceAssemblyFilePath);
+        var assembly = Assembly.LoadFrom(sourceAssemblyFilePath);
+        var assemblyFileVersion = assembly.GetCustomAttribute<AssemblyFileVersionAttribute>();
+        if (assemblyFileVersion == null) return;
+
+        var sourceDirPath = Path.GetDirectoryName(sourceAssemblyFilePath);
+        var targetZipFileName = string.Format("{0}-{1}.zip", assemblyName, assemblyFileVersion.Version);
+        var targetZipFilePath = Path.Combine(targetDirPath, targetZipFileName);
+
+        Directory.CreateDirectory(targetDirPath);
+        File.Delete(targetZipFilePath);
+        ZipFile.CreateFromDirectory(sourceDirPath, targetZipFilePath);
+    }
+
+    public static void CreateZip(string sourceDirPath, string targetZipFilePath)
+    {
+        var targetDirPath = Path.GetDirectoryName(targetZipFilePath);
+        Directory.CreateDirectory(targetDirPath);
+        File.Delete(targetZipFilePath);
+        ZipFile.CreateFromDirectory(sourceDirPath, targetZipFilePath);
+    }
+}
