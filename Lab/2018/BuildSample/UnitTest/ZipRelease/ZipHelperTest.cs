856319b05e5f6bf54691f0b057a73143e4fd190f
Keiho Sakapon <git@saka-pon.net>
2018-01-22 11:30:54 +0900

    Add test

---
+++ Lab/2018/BuildSample/UnitTest/ZipRelease/ZipHelperTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.ZipRelease
+{
+    [TestClass]
+    public class ZipHelperTest
+    {
+        [TestMethod]
+        public void CreateZipFile_1()
+        {
+            ZipHelper.CreateZipFile(@"..\Release", @"..\zip", "CreateZipFile_1.zip");
+            Assert.Inconclusive("See the file.");
+        }
+
+        [TestMethod]
+        public void CreateZipFile_2()
+        {
+            ZipHelper.CreateZipFile(@"..\Release", ".", "CreateZipFile_2.zip");
+            Assert.Inconclusive("See the file.");
+        }
+
+        [TestMethod]
+        public void CreateZipFile_3()
+        {
+            ZipHelper.CreateZipFile(".", @"..\", "CreateZipFile_3.zip");
+            Assert.Inconclusive("See the file.");
+        }
+    }
+}
