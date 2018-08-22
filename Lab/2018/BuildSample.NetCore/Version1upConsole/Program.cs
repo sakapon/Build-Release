e13efd2eb7a77f858cce51871f433a5170a355cc
Keiho Sakapon <git@saka-pon.net>
2018-08-22 17:28:17 +0900

    Create projects

---
+++ Lab/2018/BuildSample.NetCore/Version1upConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Version1upConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
