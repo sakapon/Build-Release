038f959401622e09dc65c087ad80b980103ca704
Keiho Sakapon <git@saka-pon.net>
2018-08-24 00:02:31 +0900

    Add test

---
+++ Lab/2018/BuildSample.NetCore/UnitTest/Version1up/ProgramTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UnitTest.TestHelper;
+
+namespace UnitTest.Version1up
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void IncrementForFile_1()
+        {
+            Program.IncrementForFile(@"..\..\Properties\AssemblyInfo.cs");
+            Assert.Inconclusive("See the file.");
+        }
+
+        [TestMethod]
+        public void IncrementForLine_1()
+        {
+            var Test = CreateAssertion<string, string>(Program.IncrementForLine);
+
+            Test(
+                "[assembly: AssemblyVersion(\"1.0.0.0\")]",
+                "[assembly: AssemblyVersion(\"1.0.1.0\")]");
+            Test(
+                "[assembly: AssemblyFileVersion(\"1.23.456\")]",
+                "[assembly: AssemblyFileVersion(\"1.23.457\")]");
+            Test(
+                "[assembly: AssemblyFileVersion(\"1.23.456-beta\")]",
+                "[assembly: AssemblyFileVersion(\"1.23.457-beta\")]");
+            Test(
+                "// [assembly: AssemblyVersion(\"1.0.0.0\")]",
+                "// [assembly: AssemblyVersion(\"1.0.0.0\")]");
+            Test(
+                " // [assembly: AssemblyVersion(\"1.0.0.0\")]",
+                " // [assembly: AssemblyVersion(\"1.0.0.0\")]");
+            Test(
+                "[assembly: AssemblyCompany(\"Xyz Company\")]",
+                "[assembly: AssemblyCompany(\"Xyz Company\")]");
+        }
+    }
+}
--- Lab/2018/BuildSample.NetCore/Version1upConsole/Program.cs
+++ Lab/2018/BuildSample.NetCore/Version1upConsole/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace Version1upConsole
+public static class Program
 {
-    class Program
+    public static int Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        // args[0]: The target directory path (optional).
+        var dirPath = args.Length > 0 ? args[0] : ".";
+
+        return 0;
+    }
+
+    internal static void IncrementForFile(string filePath)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static string IncrementForLine(string line)
+    {
+        throw new NotImplementedException();
     }
 }
