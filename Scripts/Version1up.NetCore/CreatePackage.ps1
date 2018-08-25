08413981944285ceddf447fb4ac62efa9540ee2e
Keiho Sakapon <git@saka-pon.net>
2018-08-25 19:46:04 +0900

    Update tools

--- Scripts/Version1up.NetCore/CreatePackage.ps1
+++ Scripts/Version1up.NetCore/CreatePackage.ps1
@@ -8,9 +8,9 @@ public static class Program
     public static int Main(string[] args)
     {
         var format = File.ReadAllText("CSharp-Format.ps1.txt", Encoding.UTF8);
-        var cs_script = File.ReadAllText(@"..\Lab\2018\BuildSample\VersionIncrementConsole\Program.cs", Encoding.UTF8);
+        var cs_script = File.ReadAllText(@"..\..\Lab\2018\BuildSample.NetCore\Version1upConsole\Program.cs", Encoding.UTF8);
         var ps_script = string.Format(format, cs_script);
-        File.WriteAllText("KTools.VersionIncrement.ps1", ps_script, Encoding.UTF8);
+        File.WriteAllText("KTools.Version1up.NetCore.ps1", ps_script, Encoding.UTF8);
 
         return 0;
     }
@@ -20,8 +20,8 @@ public static class Program
 Add-Type -TypeDefinition $source -Language CSharp
 [Program]::Main($Args)
 
-..\Tools\NuGet.CommandLine.4.4.1\NuGet.exe pack Package.nuspec
+..\..\Tools\NuGet.CommandLine.4.4.1\NuGet.exe pack Package.nuspec
 
-ni ..\Published -type directory -Force
-move *.nupkg ..\Published -Force
-explorer ..\Published
+ni ..\..\Published -type directory -Force
+move *.nupkg ..\..\Published -Force
+explorer ..\..\Published
---
+++ Scripts/Version1up.NetCore/KTools.Version1up.NetCore.ps1
@@ -0,0 +1,63 @@
+﻿$source = @"
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        // args[0]: The target directory path (optional).
+        var dirPath = args.Length > 0 ? args[0] : ".";
+
+        foreach (var filePath in GetProjFilePaths(dirPath))
+            IncrementForFile(filePath);
+
+        return 0;
+    }
+
+    static IEnumerable<string> GetProjFilePaths(string dirPath)
+    {
+        return Directory.EnumerateFiles(dirPath, "*.csproj", SearchOption.AllDirectories)
+            .Concat(Directory.EnumerateFiles(dirPath, "*.vbproj", SearchOption.AllDirectories))
+            .Concat(Directory.EnumerateFiles(dirPath, "*.fsproj", SearchOption.AllDirectories));
+    }
+
+    static readonly Encoding UTF8N = new UTF8Encoding();
+
+    internal static void IncrementForFile(string filePath)
+    {
+        Console.WriteLine(filePath);
+        var contents = File.ReadLines(filePath, UTF8N)
+            .Select(IncrementForLine)
+            .ToArray();
+        File.WriteAllLines(filePath, contents, UTF8N);
+    }
+
+    // (?<!) Zero-width negative lookbehind assertion.
+    // (?<=) Zero-width positive lookbehind assertion.
+    static readonly Regex BuildNumberPattern = new Regex(@"(?<=<(Assembly)?(File)?Version>\d+\.\d+\.)\d+");
+
+    internal static string IncrementForLine(string line)
+    {
+        var newLine = BuildNumberPattern.Replace(line, m => IncrementNumber(m.Value));
+        if (newLine != line)
+        {
+            Console.WriteLine("<< {0}", line);
+            Console.WriteLine(">> {0}", newLine);
+        }
+        return newLine;
+    }
+
+    static string IncrementNumber(string i)
+    {
+        return (int.Parse(i) + 1).ToString();
+    }
+}
+"@
+
+Add-Type -TypeDefinition $source -Language CSharp
+exit [Program]::Main($Args)
