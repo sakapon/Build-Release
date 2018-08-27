399e3917862cc541b889c1a38ee68b93aef5aee8
Keiho Sakapon <git@saka-pon.net>
2018-08-27 18:04:00 +0900

    Update tools

--- Scripts/NuGetPackup.NetCore/CreatePackage.ps1
+++ Scripts/NuGetPackup.NetCore/CreatePackage.ps1
@@ -1,26 +1,4 @@
-﻿$source = @"
-using System;
-using System.IO;
-using System.Text;
-
-public static class Program
-{
-    public static int Main(string[] args)
-    {
-        var format = File.ReadAllText("CSharp-Format.ps1.txt", Encoding.UTF8);
-        var cs_script = File.ReadAllText(@"..\..\Lab\2018\BuildSample.NetCore\ZipReleaseConsole\ZipHelper.cs", Encoding.UTF8);
-        var ps_script = string.Format(format, cs_script);
-        File.WriteAllText("KTools.ZipRelease.NetCore.ps1", ps_script, Encoding.UTF8);
-
-        return 0;
-    }
-}
-"@
-
-Add-Type -TypeDefinition $source -Language CSharp
-[Program]::Main($Args)
-
-..\..\Tools\NuGet.CommandLine.4.4.1\NuGet.exe pack Package.nuspec
+﻿..\..\Tools\NuGet.CommandLine.4.4.1\NuGet.exe pack Package.nuspec
 
 ni ..\..\Published -type directory -Force
 move *.nupkg ..\..\Published -Force
