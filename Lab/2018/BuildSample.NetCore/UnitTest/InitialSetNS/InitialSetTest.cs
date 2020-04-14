62b4a88033a24462e4326e11ccf6f921fe9f2726
Keiho Sakapon <git@saka-pon.net>
2020-04-14 10:47:05 +0900

    Update test

--- Lab/2018/BuildSample.NetCore/UnitTest/InitialSetNS/InitialSetTest.cs
+++ Lab/2018/BuildSample.NetCore/UnitTest/InitialSetNS/InitialSetTest.cs
@@ -17,51 +17,39 @@ namespace UnitTest.InitialSetNS
         [TestMethod]
         public void ReleaseGroupPattern_1()
         {
-            var Test = CreateAssertion<string, string>(s => InitialSet.ReleaseGroupPattern.Replace(s, m => "none"));
+            var Test = CreateAssertion<string, bool>(s => InitialSet.ReleaseGroupPattern.IsMatch(s));
 
             Test(
                 "  <PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' \">\r\n  <DebugType>pdbonly</DebugType>\r\n  </PropertyGroup>\r\n",
-                "  <PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' \">\r\n  <DebugType>none</DebugType>\r\n  </PropertyGroup>\r\n");
+                true);
             Test(
                 "  <PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' \">\r\n  <DebugType>full</DebugType>\r\n  </PropertyGroup>\r\n",
-                "  <PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' \">\r\n  <DebugType>full</DebugType>\r\n  </PropertyGroup>\r\n");
+                false);
             Test(
-                "<PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' \"><DebugType>pdbonly</DebugType></PropertyGroup>",
-                "<PropertyGroup Condition=\" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' \"><DebugType>none</DebugType></PropertyGroup>");
+                "<PropertyGroup Configuration == 'Release > </PropertyGroup>",
+                true);
             Test(
                 "<PropertyGroup Configuration == 'Release ><DebugType>pdbonly</DebugType></PropertyGroup><PropertyGroup Configuration == 'Release ><DebugType>full</DebugType></PropertyGroup>",
-                "<PropertyGroup Configuration == 'Release ><DebugType>none</DebugType></PropertyGroup><PropertyGroup Configuration == 'Release ><DebugType>none</DebugType></PropertyGroup>");
+                true);
         }
 
         [TestMethod]
         public void AssemblyNamePattern_1()
         {
-            var Test = CreateAssertion<string, string>(s => InitialSet.AssemblyNamePattern.Replace(s, m => ""));
+            var Test = CreateAssertion<string, string>(s => InitialSet.AssemblyNamePattern.Match(s).Value);
 
             Test(
-                "[assembly: AssemblyVersion(\"1.0.0.0\")]",
-                "[assembly: AssemblyVersion(\"1.0.0.0\")]");
+                "<AssemblyName>App123</AssemblyName>",
+                "App123");
             Test(
-                "[assembly: AssemblyFileVersion(\"1.23.456.0\")]",
-                "[assembly: AssemblyFileVersion(\"1.23.456\")]");
+                "<AssemblyName>App123",
+                "");
             Test(
-                "\r\n  [assembly: AssemblyInformationalVersion(\"12.34.56.78\")]  \r\n",
-                "\r\n  [assembly: AssemblyInformationalVersion(\"12.34.56\")]  \r\n");
+                "<AssemblyTitle>App123</AssemblyTitle>",
+                "");
             Test(
-                "[assembly: AssemblyInformationalVersion(\"1.2.3.4-alpha\")]",
-                "[assembly: AssemblyInformationalVersion(\"1.2.3\")]");
-            Test(
-                "// [assembly: AssemblyFileVersion(\"1.0.0.0\")]",
-                "// [assembly: AssemblyFileVersion(\"1.0.0.0\")]");
-            Test(
-                " // [assembly: AssemblyFileVersion(\"1.0.0.0\")]",
-                " // [assembly: AssemblyFileVersion(\"1.0.0.0\")]");
-            Test(
-                "[assembly: AssemblyCompany(\"Xyz Company\")]",
-                "[assembly: AssemblyCompany(\"Xyz Company\")]");
-            Test(
-                "\r\nAssemblyFileVersion(\"1.0.0.0\")\r\nAssemblyInformationalVersion(\"1.0.0.0\")\r\n",
-                "\r\nAssemblyFileVersion(\"1.0.0\")\r\nAssemblyInformationalVersion(\"1.0.0\")\r\n");
+                "\r\n<AssemblyName>App123</AssemblyName>\r\n<AssemblyName>App456</AssemblyName>\r\n",
+                "App123");
         }
     }
 }
